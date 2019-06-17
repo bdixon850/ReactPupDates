89452eae0ecc306ff65633d974931e794375c85d
Brandon Dixon <bdixon850@yahoo.com>
2019-06-16 23:25:16 -0400

    nav

--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -4,7 +4,18 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
     render() {
         return (
-            <div></div>
+            <div className="nav">
+                <nav>
+                    <div class="nav-wrapper">
+                    <a href="/" className="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Home</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                    </div>
+                </nav>
+            </div>
         )
     }
 };
