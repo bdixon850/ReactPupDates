5d0c6a714f292c5374b4e4df65bcb13770625719
Brandon Dixon <bdixon850@yahoo.com>
2019-06-18 12:37:50 -0400

    adding redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Navbar from './component/layout/Navbar';
+import Landing from './component/layout/Landing';
+import Register from './component/auth/Register';
+import Login from './component/auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
     render() {
         return (
-            <div className="nav">
-                <nav>
-                    <div class="nav-wrapper">
-                    <a href="/" className="brand-logo">Logo</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Home</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">JavaScript</a></li>
-                    </ul>
+            <div className="navbar-fixed">
+                <nav className="z-depth-0">
+                    <div className="nav-wrapper white">
+                        <Link
+                            to="/"
+                            style={{
+                                fontFamily: "monospace"
+                            }}
+                            className="col s5 brand-logo center black-text"
+                        >
+                            <i className="material-icons">code</i>
+                            MERN
+                        </Link>
                     </div>
                 </nav>
             </div>
-        )
+        );
     }
-};
+}
 
 export default Navbar;
